ecdbad7ef2c85e5681ad290fbaaed4587ce27652
Ian Baker <ianbaker615@gmail.com>
2021-01-10 12:55:35 -0500

    Add comments to code

--- solution.js
+++ solution.js
@@ -6,13 +6,16 @@ var findMaxConsecutiveOnes = function(nums) {
     ones_count = 0;
     highest_count = 0;
     for (num of nums) {
+        // Start counting consecutive ones
         if (num == 1) {
             ones_count += 1;
+            // Track the largest grouping of consecutive ones
             if (ones_count > highest_count) {
                 highest_count += 1;
             }
         }
         else {
+            // Reset count
             ones_count = 0
         }
     }
